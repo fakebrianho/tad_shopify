a430b9e868b5b06926d97cba6ddbb2c7d91a8568
Brian Ho <bh1525@nyu.edu>
2023-11-14 16:27:11 -0600

    admin

--- assets/index.js
+++ assets/index.js
@@ -1,23 +1,23 @@
-let imageIndex = 0 // start with the first image
-const slides = document.querySelectorAll('.slide')
+// let imageIndex = 0 // start with the first image
+// const slides = document.querySelectorAll('.slide')
 const enterPassword = document.querySelector('.nav_login')
 const form = document.querySelector('#mainForm') // Add your form's ID
-function showSlides() {
-	for (let i = 0; i < slides.length; i++) {
-		slides[i].classList.remove('active') // hide all slides
-	}
+// function showSlides() {
+// 	for (let i = 0; i < slides.length; i++) {
+// 		slides[i].classList.remove('active') // hide all slides
+// 	}
 
-	imageIndex++
-	if (imageIndex > slides.length) {
-		imageIndex = 1 // reset to first image if it's the last one
-	}
+// 	imageIndex++
+// 	if (imageIndex > slides.length) {
+// 		imageIndex = 1 // reset to first image if it's the last one
+// 	}
 
-	slides[imageIndex - 1].classList.add('active') // show current image
-	setTimeout(showSlides, 4000) // change image every 3 seconds
-}
-window.onload = function () {
-	showSlides()
-}
+// 	slides[imageIndex - 1].classList.add('active') // show current image
+// 	setTimeout(showSlides, 4000) // change image every 3 seconds
+// }
+// window.onload = function () {
+// 	showSlides()
+// }
 
 enterPassword.addEventListener('click', () => {
 	const comingSoon = document.querySelector('.content_comingSoon')
